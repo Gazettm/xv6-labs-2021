2190857ab54369db99477534987a374252ddc9bf
Gazettm <2410120028@henu.edu.cn>
2026-09-05 18:02:20 +0800

    add pingpong.c

---
+++ user/pingpong.c
@@ -0,0 +1,41 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int p2c[2], c2p[2];
+    pipe(p2c);
+    pipe(c2p);
+
+    int pid = fork();
+    if(pid == 0){
+        close(p2c[1]);
+        close(c2p[0]);
+
+        char buf[10];
+
+        read(p2c[0], buf, 4);
+        buf[4] = "\0";
+        int id = getpid();
+        printf("%d: received %s",id,buf);
+
+        write(c2p[1], "pong", 4);
+
+        exit(0);
+    }else{
+        close(p2c[0]);
+        close(c2p[1]);
+
+        char buf[10];
+
+        write(p2c[1], "ping", 4);
+
+        read(c2p[0], buf, 4);
+        buf[4] = "\0";
+        int id = getpid();
+        printf("%d: received %s",id,buf);
+        wait(0);
+        exit(0);
+    }
+}
