fd0ebc6a14a8c643133a31a99250d46fc00d2e6a
Gazettm <2410120028@henu.edu.cn>
2026-09-10 22:02:02 +0800

    vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,24 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint(pagetable_t pagetable, int depth){
+  int i,j;
+  if(depth == 0) printf("page table %p\n", pagetable);
+  for(i = 0;i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V)){
+      uint64 child = PTE2PA(pte);
+
+      for(j = 0;j <= depth;j++){
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n",i ,pte, child);
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        // 只有指向下级页表的 PTE 才递归
+        vmprint((pagetable_t)child, depth+1);
+      }
+    }
+  }
+}
