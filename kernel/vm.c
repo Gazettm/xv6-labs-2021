7cf739c882f39452154cf45a00446578e052d053
Gazettm <2410120028@henu.edu.cn>
2026-09-15 19:21:32 +0800

    copyout:va check

--- kernel/vm.c
+++ kernel/vm.c
@@ -375,6 +375,8 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
   pte_t *pte;
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
+    if(va0 >= MAXVA)
+      return -1;
     pte = walk(pagetable, va0, 0);
     if(pte == 0 || (*pte & PTE_V) == 0 || (*pte & PTE_U) == 0){
       return -1;
